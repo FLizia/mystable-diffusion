ac573639f4343dce29c2dbd5474a08c35f2b57df
FLizia <1874687961@qq.com>
2026-01-07 12:00:47 +0800

    5

--- src/taming-transformers/taming/modules/transformer/mingpt.py
+++ src/taming-transformers/taming/modules/transformer/mingpt.py
@@ -88,7 +88,7 @@ class CausalSelfAttention(nn.Module):
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head results side by side
 
         # output projection
         y = self.resid_drop(self.proj(y))
